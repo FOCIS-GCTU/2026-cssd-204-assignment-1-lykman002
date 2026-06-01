f8476bed6f051d366b5b015b09942518add65505
Abayateye Isaac Dornor <isaacabayateye8@gmail.com>
2026-06-01 01:18:11 +0000

    Completed initials assignment
    Implemented a function to print initials in stylized letters.

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
 
-def main():
-  pass #code goes here
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Abayateye Isaac Dornor
+# STUDENT ID: 2425402874
+# Email: 2425402874@live.gctu.edu.gh
+# Grader: Mr.Augustus Buckman
+#
+# On my honor, Abayateye Isaac Dornor, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
+
+def main():
+    # Print my initials in small and large stylized letters.
+
+    print()
+    print("...AID")
+    print()
+
+    print("...AAAAAAAAAAAA...IIIIIIIIIIII...DDDDDDDDDDDD....")
+    print("...AAAA....AAAA...IIIIIIIIIIII...DDDD....DDDD....")
+    print("...AAAA....AAAA.......IIII.......DDDD.....DDDD...")
+    print("...AAAA....AAAA.......IIII.......DDDD......DDDD..")
+    print("...AAAAAAAAAAAA.......IIII.......DDDD......DDDD..")
+    print("...AAAAAAAAAAAA.......IIII.......DDDD......DDDD..")
+    print("...AAAA....AAAA.......IIII.......DDDD......DDDD..")
+    print("...AAAA....AAAA.......IIII.......DDDD.....DDDD...")
+    print("...AAAA....AAAA...IIIIIIIIIIII...DDDD....DDDD....")
+    print("...AAAA....AAAA...IIIIIIIIIIII...DDDDDDDDDDDD....")
+
+    print()
 
 
 main()
